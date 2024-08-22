4ab46566982a2ab2bb34d6f5159424436a62a722
Michel John <michel.santos@dataside.com.br>
2024-08-21 21:11:51 -0300

    update total_votes on mayors_survey_results

--- src/services/supabase/queries/mayors_votes.ts
+++ src/services/supabase/queries/mayors_votes.ts
@@ -1,9 +1,16 @@
 import { supabase } from "../config";
+import { updateMayorsSurveyResults } from "./mayors_survey_results";
 
 export async function updateMayorVote(id: number, oldId?: number) {
   if (oldId) {
-    await supabase.rpc('decrement', { row_id: oldId });
+    decrementMayorVote(oldId)
+    updateMayorsSurveyResults.decrement();
   }
 
   await supabase.rpc('increment', { row_id: id });
+  await updateMayorsSurveyResults.increment();
+}
+
+async function decrementMayorVote(oldId: number) {
+  await supabase.rpc('decrement', { row_id: oldId });
 }
